77041683b7e1dd92e6c385371c661532bd714da2
Bishal Shrestha <136599301+bigketa10@users.noreply.github.com>
2026-03-18 19:14:32 +0000

    Update layout.tsx

--- app/app/layout.tsx
+++ app/app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <ClerkProvider fallbackRedirectUrl="/" signUpFallbackRedirectUrl="/">
+        <ClerkProvider signInFallbackRedirectUrl="/" signUpFallbackRedirectUrl="/">
           <ConvexClientProvider>{children}</ConvexClientProvider>
         </ClerkProvider>
       </body>
